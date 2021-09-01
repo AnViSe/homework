6ec61b3febde80b432132db2d05922b234d42a6b
AnViSe <anvise@grodno.belpost.by>
2021-09-01 22:33:15 +0300

    Рассчет баланса фигур

--- chess_fen/FEN.py
+++ chess_fen/FEN.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Напишите программу, которая использует шахматную нотацию Форсайта-Эдвардса (FEN - Forsyth–Edwards Notation)
+Напишите программу, которая использует шахматную нотацию Форсайта-Эдвардса (FEN -  )
 для подсчёта баланса материала между белыми и чёрными.
 
 - https://en.wikipedia.org/wiki/Forsyth–Edwards_Notation
@@ -18,5 +18,30 @@ ROOK_VAL = 5  # ладья
 QUEEN_VAL = 9  # ферзь
 
 
+def get_char_value(c: str) -> int:
+    """Получение ценности одной фигуры"""
+    # k - коэффициент
+    # для белых = 1, для черных = -1, для остальных = 0
+    k = c.isupper() - c.islower()
+    # Определяем ценность фигуры от ее вида и цвета
+    if c in 'pP':
+        return PAWN_VAL * k
+    elif c in 'rR':
+        return ROOK_VAL * k
+    elif c in 'bnBN':
+        return KNIGHT_VAL * k
+    elif c in 'qQ':
+        return QUEEN_VAL * k
+    else:
+        return 0
+
+
 def calc_chess_balance(fen: str) -> int:
-    pass
+    result = 0
+    # Отбираем только позиции фигур
+    lines = fen.split(' ')[0]
+    # Перебираем посимвольно
+    for c in lines:
+        # Подсчитываем общую ценность фигур
+        result = result + get_char_value(c)
+    return result
