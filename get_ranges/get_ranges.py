bba41ed521c789011c0149d79ceb7e90fa84eec7
AnViSe <anvise@grodno.belpost.by>
2021-09-19 11:01:50 +0300

    6. Functions: rectangles, get_ranges

--- get_ranges/get_ranges.py
+++ get_ranges/get_ranges.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 """
-Реализовать функцию `get_ranges` которая “сворачивает” список неповторяющихся целых чисел, отсортированных по возрастанию:
+Реализовать функцию `get_ranges` которая “сворачивает” список неповторяющихся целых чисел,
+отсортированных по возрастанию:
 
 - [0, 1, 2, 3, 4, 7, 8, 10] -> "0-4, 7-8, 10"
 - [4, 7, 10] -> "4, 7, 10"
@@ -10,4 +11,20 @@
 
 
 def get_ranges(l: list) -> str:
-    pass
+    result = []
+    _range = []
+
+    def append_to_result(e=[]):
+        if e:
+            result.append(f'{e[0]}-{e[-1]}' if e[0] != e[-1] else f'{e[0]}')
+
+    for i in range(len(l)):
+        if _range:
+            if _range[-1] + 1 != l[i]:
+                append_to_result(_range)
+                _range.clear()
+        _range.append(l[i])
+
+    append_to_result(_range)
+
+    return ", ".join(result)
